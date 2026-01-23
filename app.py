a3b91fb23fb2d9f519b58255f34858faf7600725
Gaurav Chudasama <114723172+Gaurav1504333@users.noreply.github.com>
2026-01-23 15:36:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1762,9 +1762,3 @@ def submit_review():
 
 
 #E:\wow\python.exe e:\wow\app.py 
-from flask import send_from_directory
-
-@app.route('/sitemap.xml')
-def sitemap():
-    return send_from_directory('static', 'sitemap.xml', mimetype='application/xml')
-
